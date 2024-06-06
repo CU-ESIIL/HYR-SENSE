27445a038f91728cb31eb8a154418a20e9816b9f
serbinsh <shawn.p.serbin@nasa.gov>
2024-06-06 21:24:21 +0000

    Updates

--- tools/functions/spectral_index.py
+++ tools/functions/spectral_index.py
@@ -24,7 +24,7 @@ def normalized_diff(input_xarray, band1=650, band2=850):
     return(ndiff_image)
 
 
-def pri2(input_xarray, band1=531, band2=570):
+def pri(input_xarray, band1=531, band2=570, scaled=True):
     """
     This function takes an input orthorectified EMIT xarray image and calculates a Photochemical Reflectance Index (PRI)
     image based on the selected bands. This function outputs the PRI2, which is the PRI scaled on a 0-1 scale
@@ -38,12 +38,45 @@ def pri2(input_xarray, band1=531, band2=570):
     param: band1 the EMIT band number to use for band 1
     
     param: band2 the EMIT band number to use for band 2
+
+    param: scaled scale the output pri
     """
     
     reflb1 = input_xarray.sel(wavelengths=band1, method='nearest')
     reflb2 = input_xarray.sel(wavelengths=band2, method='nearest')
     pri = (reflb1-reflb2)/(reflb1+reflb2)
-    pri2_output = (pri+1)/2
-    return(pri2_output)
+
+    if scaled == True:
+        pri = (pri+1)/2
+        
+    return(pri)
 
 
+def simple_ratio(input_xarray, band1=900, band2=970):
+    """
+    This function takes an input orthorectified EMIT xarray image and calculates a simple ration index image based on
+    the selected bands. The assumption is the input image is an EMIT image in xarray 
+    format prepared using the emit_xarray() function from emit_tools.
+
+    Function parameters:
+    param: input_xarray is the input EMIT xarray image currently located in memory; at present this will not accept 
+    the full path to a file that hasn't been loaded into memory yet (TODO)
+    
+    param: band1 the EMIT band number to use for band 1
+    
+    param: band2 the EMIT band number to use for band 2
+    """
+    
+    reflb1 = input_xarray.sel(wavelengths=band1, method='nearest')
+    reflb2 = input_xarray.sel(wavelengths=band2, method='nearest')
+    sr = reflb1/reflb2
+
+    return(sr)
+
+
+
+
+
+
+
+    
